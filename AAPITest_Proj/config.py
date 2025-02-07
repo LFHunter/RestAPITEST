abd110e41dd1bf7d97eb767befb69934b4ce78f3
LFHunter <magicryze@gmail.com>
2025-02-07 22:28:50 +0800

    add Baselib and Project

---
+++ AAPITest_Proj/config.py
@@ -0,0 +1,33 @@
+import datetime
+now = datetime.datetime.now()
+formatted_now = now.strftime("%Y-%m-%d_%H%M_%S")
+
+log_config = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'level': 'DEBUG'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': f'{formatted_now}_twitch.log',
+            'formatter': 'standard',
+            'level': 'DEBUG'
+        }
+    },
+    'loggers': {
+        '': {  # '' means root logger
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
+    }
+}
