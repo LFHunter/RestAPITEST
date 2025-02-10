0acacc9f3b7c0eebb3af8957c86c8bbf7e296682
unknown <magicryze@gmail.com>
2025-02-10 23:27:55 +0800

    add logger  type lint

--- Baselib/logger_manager.py
+++ Baselib/logger_manager.py
@@ -1,7 +1,7 @@
-import logging
-import json
-import logging.config
 from logging import Logger
+import logging
+import logging.config
+import json
 
 
 def setup_logger(log_config: json, loggername: str) -> Logger:
