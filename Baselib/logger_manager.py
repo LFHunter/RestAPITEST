5080c38e170895aaf56902e082666aba4651c095
unknown <magicryze@gmail.com>
2025-02-10 15:12:42 +0800

    add logger and fix test_historical_api.py error

--- Baselib/logger_manager.py
+++ Baselib/logger_manager.py
@@ -1,7 +1,10 @@
 import logging
+import json
 import logging.config
+from logging import Logger
 
-def setup_logger(log_config,loggername):
+
+def setup_logger(log_config: json, loggername: str) -> Logger:
     logging.config.dictConfig(log_config)
     logger = logging.getLogger(loggername)
     return logger
